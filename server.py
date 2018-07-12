07b54d37bbd06d575de8482bf0a72899b05756ee
AhmedZara <zara586243@gmail.com>
2018-07-12 17:37:12 +0530

    ui changes

--- server.py
+++ server.py
@@ -3,5 +3,10 @@ app=Flask(__name__)
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
+@app.route('/contactme',methods=['GET','POST'])
+def contactme():
+   
+   return jsonify({"firstname":request.form['firstname'], "lastname":request.form['lastname']})
+
 if __name__=='__main__':
     app.run(debug=True)
