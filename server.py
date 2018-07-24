b0da54f2928678283ff8547d70e468d2bd5c2bcc
AhmedZara <zara586243@gmail.com>
2018-07-24 14:52:44 +0530

    edited portfolio

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
-from flask import Flask,request,jsonify,render_template
+from flask import Flask,request,jsonify,render_template, url_for
+
 app=Flask(__name__)
+
 @app.route('/',methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/landing',methods=['GET'])
+def landing():
+    return render_template('landing.html')
+
 @app.route('/contactme',methods=['GET','POST'])
-def contactme():
-   
+def contactme(): 
    return jsonify({"firstname":request.form['firstname'], "lastname":request.form['lastname']})
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
